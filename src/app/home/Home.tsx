9ef324e2f030e755830223da73aa6b8b7a2576c0
Wes Lord <weslord@gmail.com>
2022-03-07 19:51:36 -0800

    Add example notification

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -2,10 +2,20 @@ import { store } from 'store/store'
 
 import { Button } from 'elements/button/Button'
 import { modalsActions } from 'store/slices/modals'
+import { notificationsActions } from 'store/slices/notifications'
 
 export const Home = () => {
     const openModal = () => {
         store.dispatch(modalsActions.open({ modalType: 'SOME_MODAL' }))
+    }
+
+    const createNotification = () => {
+        store.dispatch(
+            notificationsActions.addNotification({
+                type: 'success',
+                text: 'Here is your notification',
+            })
+        )
     }
 
     return (
@@ -13,6 +23,9 @@ export const Home = () => {
             <h1>Home</h1>
             <div>
                 <Button onClick={openModal}>Open Modal</Button>
+                <Button onClick={createNotification}>
+                    Create Notification
+                </Button>
             </div>
         </div>
     )
